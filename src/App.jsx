cfe879ed6b6a71e6fcb676c59b93e3679366dfae
Hugo Machefer <hugo.machefer@gmail.com>
2026-02-27 21:11:16 -0500

    first commit to settle the fundations of our basic vanilla html form

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import YoutubeForm from './components/YoutubeForm'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <YoutubeForm />
+    </>
+  )
+}
+
+export default App
