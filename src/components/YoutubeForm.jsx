0a25597d978f1f76dd0104981d34af7b598b938f
Hugo Machefer <hugo.machefer@gmail.com>
2026-03-01 19:54:16 -0500

    add formik as a node module, create YoutubeForm component with companion css file, and import it in App.jsx main file

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -1,22 +1,41 @@
 import React from 'react'
+import { useFormik } from 'formik';
+import './YoutubeForm.css';
 
 function YoutubeForm() {
+
+  const formik = useFormik({
+    initialValues: {
+        name: 'Hugo',
+        email: '',
+        channel: ''
+    }
+  });
+
+  console.log('Form values', formik.values);
+
   return (
-    <div>
+    <div className='form-container'>
         <form>
-            <label htmlFor='name'>Name</label>
-            <input type='text' id='name' name='name' />
+            <div className='form-field'>
+                <label htmlFor='name'>Name</label>
+                <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name} />
+            </div>
 
-            <label htmlFor='email'>E-mail</label>
-            <input type='email' id='email' name='email' />
+            <div className='form-field'>
+                <label htmlFor='email'>E-mail</label>
+                <input type='email' id='email' name='email' onChange={formik.handleChange} value={formik.values.email}/>
+            </div>
 
-            <label htmlFor='channel'>Channel</label>
-            <input type='text' id='channel' name='channel' />
+            <div className='form-field'>
+                <label htmlFor='channel'>Channel</label>
+                <input type='text' id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel}/>
+            </div>
 
-            <button>Submit</button>
+            <button type='button'>Submit</button>
         </form>
-        </div>
-    )
+    </div>
+  )
 }
 
 export default YoutubeForm
