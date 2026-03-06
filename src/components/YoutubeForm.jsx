77ba9523f2c69dc02fa67840bc3545639ad73a0e
Hugo Machefer <hugo.machefer@gmail.com>
2026-03-06 15:40:03 -0500

    feat(YoutubeForm): show errors only on visited fields using formik.touched
    - Add onBlur={formik.handleBlur} to all input fields
    - Log formik.touched to track visited fields
    - Conditionally render errors only after field has been touched

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -39,27 +39,45 @@ function YoutubeForm() {
     validate
   });
 
-  console.log('Form errors', formik.errors);
+  //  console.log('Form values', formik.values);
+  //  console.log('Form errors', formik.errors);
+  console.log('Visited fields', formik.touched);
+
 
   return (
     <div className='form-container'>
         <form onSubmit={formik.handleSubmit}>
             <div className='form-field'>
                 <label htmlFor='name'>Name</label>
-                <input type='text' id='name' name='name' onChange={formik.handleChange} value={formik.values.name} />
-                {formik.errors.name ? <div style={{color: 'red'}}>{formik.errors.name}</div> : null}
+                <input type='text' id='name' name='name' 
+                onChange={formik.handleChange} 
+                onBlur={formik.handleBlur}
+                value={formik.values.name} />
+                {formik.errors.name 
+                ? <div style={{color: 'red'}}>{formik.errors.name}</div> 
+                : null}
             </div>
 
             <div className='form-field'>
                 <label htmlFor='email'>E-mail</label>
-                <input type='email' id='email' name='email' onChange={formik.handleChange} value={formik.values.email}/>
-                {formik.errors.email ? <div style={{color: 'red'}}>{formik.errors.email}</div> : null}
+                <input type='email' id='email' name='email' 
+                onChange={formik.handleChange} 
+                onBlur={formik.handleBlur}
+                value={formik.values.email}/>
+                {formik.errors.email 
+                ? <div style={{color: 'red'}}>{formik.errors.email}</div> 
+                : null}
             </div>
 
             <div className='form-field'>
                 <label htmlFor='channel'>Channel</label>
-                <input type='text' id='channel' name='channel' onChange={formik.handleChange} value={formik.values.channel}/>
-                {formik.errors.channel ? <div style={{color: 'red'}}>{formik.errors.channel}</div> : null}
+                <input type='text' id='channel' name='channel' 
+                onChange={formik.handleChange} 
+                onBlur={formik.handleBlur}
+                value={formik.values.channel}/>
+                {formik.errors.channel 
+                ? <div style={{color: 'red'}}>{formik.errors.channel}</div> 
+                : null}
             </div>
 
             <button type='submit'>Submit</button>
